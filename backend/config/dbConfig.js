14f2994c54fa5f5c51696045a164c427bf1d67be
mustafaDitabar <mustafa3amini555@gmail.com>
2025-07-21 11:02:23 +0430

    2

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
 const connectDB = async () => {
+  console.log("📡 Connecting to MongoDB with URI:", process.env.DATABASE_URL);
   try {
     await mongoose.connect(process.env.DATABASE_URL, {
       useUnifiedTopology: true,
